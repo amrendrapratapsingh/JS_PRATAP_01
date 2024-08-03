de9f0e2fb5910f775253b8c2a1f7d84d05e46cc8
Amrendra <amrendrapratapcoders@gmail.com>
2024-08-04 02:05:41 +0530

    variable declaration

---
+++ pratap_01/varaible.js
@@ -0,0 +1,18 @@
+const accountId = 4563789;
+let accountEmail="pratapjs@gmail.com";
+var accountpwd = "78963";
+    accountCity="varanasi";
+
+    let accounstate;
+
+    // accountId = 56789; // const value can be reassigned and redeclare.
+
+    // let keyword can be reassigned but can not redeclare.
+
+    // var keyword can be reassigned and redeclare also.
+
+    // prefer don't use var keyword beacuse issues in block scope and functional scope.
+
+    // console.log(accountId);
+
+    console.table([accountId,accountEmail,accountpwd,accounstate]);
