4c72dcb33de09e8c734ce9801b3cd38585f3dcce
Amrendra <amrendrapratapcoders@gmail.com>
2024-08-04 02:58:40 +0530

    Data Conversion

---
+++ pratap_01/converionsDeclaration.js
@@ -0,0 +1,30 @@
+let score = "33pratap";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+
+let valueNumber = Number(score);
+// console.log(typeof valueNumber);
+// console.log(valueNumber);
+
+
+// "33" => 33
+
+// "33Pratap" => NAN
+// true =>1; false =>0
+
+let isPrataploggedIn = "Pratap"
+
+
+let booleanIsLoggedIn = Boolean(isPrataploggedIn);
+
+console.log(typeof booleanIsLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// "" => false
+
+
+// 1 => true; 0=> false
+// "Pratap" => true
