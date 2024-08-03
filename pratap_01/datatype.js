5a7ae040942fcafbe0ddf3e1beadd7694a8507e6
Amrendra <amrendrapratapcoders@gmail.com>
2024-08-04 02:33:18 +0530

    Data type declaration

---
+++ pratap_01/datatype.js
@@ -0,0 +1,20 @@
+let name = "Amrendra Pratap"
+let city = "Bangalore"
+let isloggedIn = false
+
+// number => 2 to power 53
+// biginit => for large number
+// string => ""
+// boolean => true/false
+// null => standalone vale
+// undefine
+// symbol => for unique
+
+
+// Non Primitive data type
+
+// Object
+
+
+console.log(typeof null); // object
+console.log(typeof undefine); // undefine
